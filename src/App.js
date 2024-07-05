8430aac1904464b1f07aeb82fc7e0806079aea4b
kshitijbahul <chunks.3@gmail.com>
2024-07-05 14:46:40 +0200

    feat: used class instead of functions(hooks) to reformat later

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-// Component is a function that returns an JSX element(JS extende code) 
+class App extends Component {
+  // Called anytime the componnet is created
+  constructor(){
+    super();
+    this.state = {
+      name: 'Rahul'
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h2>My Name is Kshitij</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Component is a function that returns an JSX element(JS extende code) 
+  //The render method already exists on the Component class, 
+  // We change what we pass to the function to render
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <h2>My Name is {this.state.name}</h2>
+          <button>Change name</button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
