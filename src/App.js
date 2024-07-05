6181bd752e5b5bc12dde381d058fc5afe98edf20
kshitijbahul <chunks.3@gmail.com>
2024-07-05 16:17:08 +0200

    feat: used map to display similar information

--- src/App.js
+++ src/App.js
@@ -8,8 +8,24 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      name: 'Rahul',
-      company: 'Google'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 1
+        },
+        {
+          name: 'Dracula',
+          id: 2
+        },
+        {
+          name: 'Zombie',
+          id: 3
+        },
+        {
+          name: 'Mummy',
+          id: 4
+        }
+      ]
     }
   }
 
@@ -22,34 +38,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <h2>My Name is {this.state.name} and I work at {this.state.company}</h2>
-          <button onClick={() => {
-            //Takes 2 fuctions, 
-            // 1st function is the runction which returns the state to be updated and provides 2 arguments, state(i.e current state before update) and props
-            // 2nd function is a callback function which is called after the state is updated. it is optional
-            this.setState((state, props) => {
-              return {
-                name: 'John'
-              }
-            },() => {
-              console.log(this.state)
-            })// 
-            
-          } }>Change name</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+          return <div key = {monster.id}>
+            <h1 >{monster.name}</h1>
+          </div>
+        })
+        }
       </div>
     );
   }
