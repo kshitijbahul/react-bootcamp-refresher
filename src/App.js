353461643f214ae6fb64d81441faa5db108e93f7
kshitijbahul <chunks.3@gmail.com>
2024-07-05 17:09:34 +0200

    feat: use lifecycle moethod componentDidMount

--- src/App.js
+++ src/App.js
@@ -8,26 +8,23 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 1
-        },
-        {
-          name: 'Dracula',
-          id: 2
-        },
-        {
-          name: 'Zombie',
-          id: 3
-        },
-        {
-          name: 'Mummy',
-          id: 4
-        }
-      ]
+      monsters: []
     }
+    console.log('constructor')
   }
+
+  componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState(()=> {
+        // return the state object
+        return {monsters: users}
+      },() => {
+        console.log(this.state.monsters)
+      }))
+      console.log('componentDidMount')
+  }
+    
 
   // Component is a function that returns an JSX element(JS extende code) 
   //The render method already exists on the Component class, 
@@ -36,8 +33,14 @@ class App extends Component {
     this.setState({})
   }
   render() {
+    console.log('render')
     return (
       <div className="App">
+        <input className='search-box' type ='search' placeholder='Seach here' onChange={ (event)=> {
+          this.setState(()=>{
+            return { monsters: this.state.monsters.filter((monster) => {return monster.name.includes(event.target.value)})}
+          },()=> {})
+        } }/>
         {
           this.state.monsters.map((monster) => {
           return <div key = {monster.id}>
