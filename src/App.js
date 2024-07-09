2353ddebac3aa267f5539c479235728b935f1a62
kshitijbahul <chunks.3@gmail.com>
2024-07-09 14:04:55 +0200

    feat: created first component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import  CardList from './components/card-list/card-list.component';
+
 
 import logo from './logo.svg';
 import './App.css';
@@ -52,7 +54,7 @@ class App extends Component {
         {
           filteredMosters.map((monster) => {
           return <div key = {monster.id}>
-            <h1 >{monster.name}</h1>
+            <CardList></CardList>
           </div>
         })
         }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class CardList extends Component {
+    render () {
+        return <div>
+            <h1>Cardlist Component</h1>
+        </div>
+    }
+}
+
+export default CardList;
